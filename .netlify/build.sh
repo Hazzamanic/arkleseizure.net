b28b322b3a3309ab54b26e5fdbeec235b49cb267
Hazzamanic <breakitup@hotmail.co.uk>
2020-04-27 17:32:29 +0100

    histanai version bump

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -4,6 +4,6 @@ export DOTNET_ROOT=/opt/buildhome/.dotnet
 
 sh ./.netlify/dotnet-install.sh --version 3.1.201
 
-dotnet tool install -g Histanai --version 0.0.1
+dotnet tool install -g Histanai --version 0.0.2
 
 histanai build
