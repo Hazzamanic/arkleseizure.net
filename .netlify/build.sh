fc18b7b53a3ab5bb0f7a4437d9e6b1e40c19efb4
Hazzamanic <breakitup@hotmail.co.uk>
2020-04-27 17:01:09 +0100

    execute install dotnet as script

--- .netlify/build.sh
+++ .netlify/build.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 export DOTNET_ROOT=/opt/buildhome/.dotnet
-./.netlify/dotnet-install.sh --version 3.1.201
+
+sh ./.netlify/dotnet-install.sh --version 3.1.201
+
 dotnet tool install -g Histanai -v 0.0.1
+
 histanai build
